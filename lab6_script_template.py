48e6f0c849069f49be36f625a6e33e88c531302d
JehosafatJon <jonathan.hughes@flemingcollege.ca>
2023-03-06 13:17:26 -0500

    Finished Lab Period
    Still need Clean-Up and Docstring

--- lab6_script_template.py
+++ lab6_script_template.py
@@ -1,6 +1,9 @@
 
 import requests as rq
 import hashlib
+import re
+import os
+import subprocess
 
 def main():
 
@@ -24,24 +27,48 @@ def main():
         delete_installer(installer_path)
 
 def get_expected_sha256():
+    file_url = 'http://download.videolan.org/pub/videolan/vlc/3.0.17.4/win64/vlc-3.0.17.4-win64.exe.sha256'
+    resp_msg = rq.get(file_url)
 
+    if resp_msg.status_code == rq.codes.ok:
 
+        file_content = resp_msg.text
+        hash = re.match(r'^(.*) ', file_content).groups(1)[0]
 
-    return 
+    return hash
 
 def download_installer():
+    file_url = 'http://download.videolan.org/pub/videolan/vlc/3.0.17.4/win64/vlc-3.0.17.4-win64.exe'
+    resp_msg = rq.get(file_url)
+
+    if resp_msg.status_code == rq.codes.ok:
+        return resp_msg.content
+
+
     return
 
 def installer_ok(installer_data, expected_sha256):
-    return
+    installer_hash = hashlib.sha256(installer_data).hexdigest()
+
+    if installer_hash == expected_sha256:
+        return True
+    
+    return False
 
 def save_installer(installer_data):
-    return
+    with open(os.getenv('TEMP')+'\\vlc_installer.exe', 'wb') as file:
+        file.write(installer_data)
+
+    return file.name
 
 def run_installer(installer_path):
+
+    subprocess.run([installer_path, '/L=1033', '/S'])
+
     return
     
 def delete_installer(installer_path):
+    os.remove(installer_path)
     return
 
 if __name__ == '__main__':
