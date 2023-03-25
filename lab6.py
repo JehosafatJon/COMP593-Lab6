a30b2cf6cffee46f263c599ef2ae9e2b02a59b38
JehosafatJon <122817924+JehosafatJon@users.noreply.github.com>
2023-03-25 15:18:41 -0400

    Added Docstrings

--- lab6.py
+++ lab6.py
@@ -1,3 +1,14 @@
+"""
+Description:
+A script that downloads and silently runs
+a VLC media player installer file, verified by
+hash values. The installer file is discarded after use.
+
+Usage:
+python lab6.py
+
+"""
+
 import requests as rq
 import hashlib
 import os
@@ -25,6 +36,13 @@ def main():
         delete_installer(installer_path)
 
 def get_expected_sha256():
+    """
+    Fetches the expected hash value from the videolan website
+
+    Returns:
+        str: the expected SHA256 hash of the installer file
+    """    
+
     file_url = 'http://download.videolan.org/pub/videolan/vlc/3.0.17.4/win64/vlc-3.0.17.4-win64.exe.sha256'
     resp_msg = rq.get(file_url)
 
@@ -36,6 +54,13 @@ def get_expected_sha256():
     return hash
 
 def download_installer():
+    """
+    Downloads the VLC installer and returns the data of the installer
+
+    Returns:
+        byte str: The binary data of the installer file
+    """    
+    
     file_url = 'http://download.videolan.org/pub/videolan/vlc/3.0.17.4/win64/vlc-3.0.17.4-win64.exe'
     resp_msg = rq.get(file_url)
 
@@ -45,6 +70,18 @@ def download_installer():
     return
 
 def installer_ok(installer_data, expected_sha256):
+    """
+    Checks that the hash value of the downloaded file
+    matches the expected hash value provided by the VLC website.
+
+    Args:
+        installer_data (byte str): The binary data of the installer file
+        expected_sha256 (str): The expected hash value of the installer file
+
+    Returns:
+        bool: True/False indicator if the hash values match
+    """    
+    
     installer_hash = hashlib.sha256(installer_data).hexdigest()
 
     if installer_hash == expected_sha256:
@@ -53,19 +90,41 @@ def installer_ok(installer_data, expected_sha256):
     return False
 
 def save_installer(installer_data):
-    
+    """
+    Writes the binary data of the installer executable to a file
+    and returns the path of the file.
+
+    Args:
+        installer_data (byte str): The binary data of the installer
+
+    Returns:
+        str: The path of the installer file
+    """    
+
     with open(os.getenv('TEMP')+'\\vlc_installer.exe', 'wb') as file:
         file.write(installer_data)
 
     return file.name
 
 def run_installer(installer_path):
-    
+    """
+    Silently runs the installer executable file.
+
+    Args:
+        installer_path (str): The path of the installer file
+    """    
+
     subprocess.run([installer_path, '/L=1033', '/S'])
 
     return
     
 def delete_installer(installer_path):
+    """
+    Deletes the installer executable file
+
+    Args:
+        installer_path (str): The path of the installer
+    """    
 
     os.remove(installer_path)
     
